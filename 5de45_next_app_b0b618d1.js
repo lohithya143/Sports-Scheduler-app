24b1c507027fc4e65774c697a6d83adb17d7d0ef
lohithya143 <lohithya77@gmail.com>
2025-10-02 11:26:36 +0530

    Add files via upload

---
+++ 5de45_next_app_b0b618d1.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/OneDrive/Desktop/part/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+module.exports = __turbopack_context__.r("[project]/OneDrive/Desktop/part/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
+}),
+]);
+
+//# sourceMappingURL=5de45_next_app_b0b618d1.js.map
