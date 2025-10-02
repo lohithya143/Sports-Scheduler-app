549e869e7ecf0b7a61eff57e56b0dc7a736a5d06
lohithya143 <lohithya77@gmail.com>
2025-10-02 11:33:35 +0530

    Add files via upload

---
+++ _error.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
+R.c("server/chunks/ssr/_15a88f23._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__fa248b9f._.js")
+R.c("server/chunks/ssr/5de45_next_bd6b0fb8._.js")
+R.c("server/chunks/ssr/5de45_4fdf02cf._.js")
+R.c("server/chunks/ssr/[externals]_next_dist_shared_lib_no-fallback-error_external_59b92b38.js")
+R.m("[project]/OneDrive/Desktop/part/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/OneDrive/Desktop/part/node_modules/next/error.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/OneDrive/Desktop/part/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/OneDrive/Desktop/part/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
+module.exports=R.m("[project]/OneDrive/Desktop/part/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/OneDrive/Desktop/part/node_modules/next/error.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/OneDrive/Desktop/part/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/OneDrive/Desktop/part/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
---
+++ middleware-build-manifest.js
@@ -0,0 +1,55 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": [
+      "static/chunks/5de45_next_dist_compiled_next-devtools_index_8982ca25.js",
+      "static/chunks/5de45_next_dist_compiled_5a53ed7c._.js",
+      "static/chunks/5de45_next_dist_shared_lib_bc5d4de7._.js",
+      "static/chunks/5de45_next_dist_client_1bc2acb3._.js",
+      "static/chunks/5de45_next_dist_b8f0aef8._.js",
+      "static/chunks/5de45_next_app_b0b618d1.js",
+      "static/chunks/[next]_entry_page-loader_ts_bf0d50b5._.js",
+      "static/chunks/5de45_react-dom_1199ab36._.js",
+      "static/chunks/5de45_c0bc3e2d._.js",
+      "static/chunks/[root-of-the-server]__653c0dbc._.js",
+      "static/chunks/OneDrive_Desktop_part_src_pages__app_2da965e7._.js",
+      "static/chunks/turbopack-OneDrive_Desktop_part_src_pages__app_7f3b3174._.js"
+    ],
+    "/_error": [
+      "static/chunks/5de45_next_dist_compiled_next-devtools_index_8982ca25.js",
+      "static/chunks/5de45_next_dist_compiled_5a53ed7c._.js",
+      "static/chunks/5de45_next_dist_shared_lib_7b6bc89c._.js",
+      "static/chunks/5de45_next_dist_client_1bc2acb3._.js",
+      "static/chunks/5de45_next_dist_f39661f0._.js",
+      "static/chunks/5de45_next_error_4593af5f.js",
+      "static/chunks/[next]_entry_page-loader_ts_ddf5e67c._.js",
+      "static/chunks/5de45_react-dom_1199ab36._.js",
+      "static/chunks/5de45_c0bc3e2d._.js",
+      "static/chunks/[root-of-the-server]__7a978c9d._.js",
+      "static/chunks/OneDrive_Desktop_part_src_pages__error_2da965e7._.js",
+      "static/chunks/turbopack-OneDrive_Desktop_part_src_pages__error_b32092d2._.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/5de45_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_42460ac5._.js",
+    "static/chunks/5de45_next_dist_compiled_react-dom_df4395af._.js",
+    "static/chunks/5de45_next_dist_compiled_next-devtools_index_96878175.js",
+    "static/chunks/5de45_next_dist_compiled_3cff5ae0._.js",
+    "static/chunks/5de45_next_dist_client_be4e1a57._.js",
+    "static/chunks/5de45_next_dist_67a02127._.js",
+    "static/chunks/5de45_@swc_helpers_cjs_16b94034._.js",
+    "static/chunks/OneDrive_Desktop_part_a0ff3932._.js",
+    "static/chunks/turbopack-OneDrive_Desktop_part_c15da25b._.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
